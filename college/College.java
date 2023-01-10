7f9f1c58ac2795ea9d04809321278b688b23e988
sharmi-2014 <121440788+sharmi-2014@users.noreply.github.com>
2023-01-10 23:13:07 +0530

    Add files via upload

---
+++ college/College.java
@@ -0,0 +1,16 @@
+package org.college;
+
+public class College {
+	public void collegeName() {
+		System.out.println("AAMEC");
+	}
+		public void collegeCode() {
+			System.out.println("01223");
+		}
+		public void collegeRank() {
+			System.out.println("15");
+		}
+		
+	}
+
+
